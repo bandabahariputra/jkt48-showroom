b4307a2b219d3c217208f96cd7d109fcefa131c5
Banda Bahari Putra <bandabahariputra99@gmail.com>
2023-02-07 01:53:48 +0700

    Add academy members

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import useFetch from './useFetch';
 import Title from './Title';
 import Card from './Card';
@@ -6,6 +7,42 @@ import Skeleton from './Skeleton';
 
 const Home = () => {
   const [members, setMembers] = useState(null);
+  const [academyMembers, setAcademyMembers] = useState(null);
+
+  const academyMemberIds = [
+    {
+      id: '400710',
+      name: 'amanda',
+    },
+    {
+      id: '400712',
+      name: 'giselle',
+    },
+    {
+      id: '400713',
+      name: 'lia',
+    },
+    {
+      id: '400714',
+      name: 'callie',
+    },
+    {
+      id: '400715',
+      name: 'ela',
+    },
+    {
+      id: '400716',
+      name: 'indira',
+    },
+    {
+      id: '400717',
+      name: 'lyn',
+    },
+    {
+      id: '400718',
+      name: 'raisha',
+    },
+  ];
 
   const { data: regularMembers } = useFetch(
     'https://api.codetabs.com/v1/proxy/?quest=https://campaign.showroom-live.com/akb48_sr/data/room_status_list.json',
@@ -26,21 +63,58 @@ const Home = () => {
     }
   }, [regularMembers]);
 
+  useEffect(() => {
+    // academy members
+    const getAcademyMembers = async () => {
+      const acms = await Promise.all(academyMemberIds.map(async (academy) => {
+        const { data: acm } = await axios.get(`https://api.codetabs.com/v1/proxy/?quest=https://www.showroom-live.com/api/room/profile?room_id=${academy.id}`);
+
+        return acm;
+      }));
+
+      setAcademyMembers(acms.map((item) => ({
+        description: item.description,
+        follower_num: item.follower_num,
+        id: item.room_id,
+        image_url: item.image,
+        is_live: item.is_onlive,
+        is_party: item.party_live_status === 1,
+        name: item.room_name,
+        next_live_scheduled: null,
+        url_key: item.room_url_key,
+      })));
+    };
+
+    getAcademyMembers();
+  }, []);
+
   return (
-    <>
+    <div className="flex flex-col space-y-8">
       {members ? (
-        <>
+        <div>
           <Title text="Regular" />
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {members.map((member) => (
               <Card key={member.id} data={member} to={`/room/${member.id}`} />
             ))}
           </div>
-        </>
+        </div>
       ) : (
         <Skeleton />
       )}
-    </>
+      {academyMembers ? (
+        <div>
+          <Title text="Academy" />
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {academyMembers.map((member) => (
+              <Card key={member.id} data={member} to={`/room/${member.id}`} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Skeleton />
+      )}
+    </div>
   );
 };
 
